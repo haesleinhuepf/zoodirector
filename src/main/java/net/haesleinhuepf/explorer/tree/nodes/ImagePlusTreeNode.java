e3c7f837353c2009fcca2e387b84b0cb1250645c
haesleinhuepf <rhaase@mpi-cbg.de>
2020-01-01 16:35:02 +0100

    bugfix: disabled old imageplus code which was duplicating stacks

--- src/main/java/net/haesleinhuepf/explorer/tree/nodes/ImagePlusTreeNode.java
+++ src/main/java/net/haesleinhuepf/explorer/tree/nodes/ImagePlusTreeNode.java
@@ -14,70 +14,14 @@ import net.imglib2.util.Intervals;
 import javax.swing.*;
 import java.nio.charset.Charset;
 
-public class ImagePlusTreeNode extends AbstractTreeNode implements ImageListener, PropertiesManipulatable {
+public class ImagePlusTreeNode extends AbstractTreeNode implements PropertiesManipulatable {
 	ImagePlus imp;
 
 	public ImagePlusTreeNode(JTree tree, ImagePlus imp, AbstractTreeNode parent) {
 		super(tree, imp.getTitle(), parent);
 		this.imp = imp;
 
-		imp.addImageListener(this);
-	}
 
-	@Override
-	public void imageOpened(ImagePlus imp) {
-	}
-
-	@Override
-	public void imageClosed(ImagePlus imp) {
-		System.out.println("image closed: " + imp);
-		if (imp != this.imp) {
-			return;
-		}
-		
-		// Dirty workaround, because ImageJ vanishes the object even though I'm holding it :(
-		this.imp = new Duplicator().run(imp);
-	}
-
-	@Override
-	public void imageUpdated(ImagePlus imp) {
-		if (imp != this.imp) {
-			return;
-		}
-		System.out.println("image changed: " + imp);
-		somethingChanged(imp);
-	}
-
-	private int formerZ = -1;
-
-	@Override
-	public void somethingChanged(Object o) {
-		System.out.println("something changed!");
-		boolean pass = false;
-		if (o == imp) {
-			System.out.println("help, my image changed!");
-			pass = true;
-		}
-		if (!pass && o instanceof AbstractTreeNode)
-		{
-			AbstractTreeNode tn = (AbstractTreeNode)o;
-			for (int j = 0; j < getChildCount(); j++)
-			{
-				System.out.println("eq: " + getChildAt(j) + " == " + o);
-				if (getChildAt(j) == tn)
-				{
-					System.out.println("help, onf of my children changed!");
-					pass = true;
-					break;
-				}
-			}
-		}
-		if (!pass)
-		{
-			return;
-		}
-		
-		redrawOverLay();
 	}
 
 	@Override
@@ -95,20 +39,6 @@ public class ImagePlusTreeNode extends AbstractTreeNode implements ImageListener
 
 	public ImagePlus getImagePlus() {
 		return imp;
-	}
-
-	private void redrawOverLay() {
-		Overlay ov = new Overlay();
-		
-		Interval interval = Intervals.createMinMax(0,0,imp.getZ()-1, imp.getWidth(), imp.getHeight(), imp.getZ()-1);
-		
-		for (int j = 0; j < getChildCount(); j++) {
-			Object treeSubNode = getChildAt(j);
-			if (treeSubNode instanceof OverlayUpdater) {
-				((OverlayUpdater) treeSubNode).updateOverlay(ov, interval);
-			}
-		}
-		imp.setOverlay(ov);
 	}
 
 	@Override
